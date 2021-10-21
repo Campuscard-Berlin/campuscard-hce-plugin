ccdcbd08f09785c0c40a66e4383ba5ba169b8083
Martin Aigner <aignemar@hu-berlin.de>
2021-10-21 11:33:11 +0200

    Move to using wrapped Desfire commands

--- android/src/main/java/berlin/campuscard/hce/se/Emulation.java
+++ android/src/main/java/berlin/campuscard/hce/se/Emulation.java
@@ -14,7 +14,22 @@ public class Emulation {
 
     public byte[] getResponse(byte[] apdu) {
         Log.i(TAG, "getResponse()");
-        return secureElement.processCommand(new Command(apdu));
+
+        byte[] unwrappedApdu = new byte[apdu.length - 5 <= 0 ? 1 : apdu.length - 5];
+        unwrappedApdu[0] = apdu[1];
+
+        if (apdu.length > 5) {
+          System.arraycopy(apdu, 5, unwrappedApdu, 1, apdu.length - 6);
+        }
+
+        byte[] unwrappedResponse = secureElement.processCommand(new Command(unwrappedApdu));
+
+        byte[] wrappedResponse = new byte[unwrappedResponse.length + 1];
+        System.arraycopy(unwrappedResponse, 1, wrappedResponse, 0, unwrappedResponse.length - 1);
+        wrappedResponse[wrappedResponse.length - 2] = (byte) 0x91;
+        wrappedResponse[wrappedResponse.length - 1] = unwrappedResponse[0];
+
+        return wrappedResponse;
     }
 
 }
